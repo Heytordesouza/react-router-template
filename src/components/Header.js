b43d9a4306c6d86eaf33a5393f21e42cec2901ff
Heytordesouza <heytordesnascimento@gmail.com>
2022-12-28 21:06:45 -0300

    React Router Aula

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
-function Header() {
+import {goToHomePage, goToProfilePage , goToLoginPage} from "../Router/coordinator"
+import { useNavigate } from "react-router-dom"
 
+function Header() {
+  const navigate = useNavigate()
   return (
     <header>
-      <button >
+      <button onClick={() => goToHomePage(navigate)}>
         Ir para página inicial
       </button>
-      <button >
+      <button onClick={() => goToProfilePage(navigate, "Heytor")}>
         Ir para página de perfil
+      </button>
+      <button onClick={() => goToLoginPage(navigate)}>
+        Ir para página de Login
       </button>
     </header>
   );
